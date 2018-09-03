bfaf281ec912642bd8c6d6faf1518fb2a2d73f70
23pieces <23pieces@protonmail.com>
2018-09-03 21:40:48 +0200

    Usability Patches to mkfw

--- efwt/efwt.c
+++ efwt/efwt.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <stdint.h>
 
-extern unsigned long crc32(unsigned long crc, const unsigned char* buf, unsigned int len);
-
 char* FIRMWARE = "firmware.fw";
 char* HEADER = "ODROIDGO_FIRMWARE_V00_01";
 
@@ -24,42 +22,56 @@ int main(int argc, char *argv[])
     else
     {
         FILE* file = fopen(argv[1], "rb");
-        if (!file) abort();
-	printf("Opened %s \n", argv[1]);
+        if (!file)
+		{
+			printf("Couldn't open %s \n", argv[1]);
+			printf("Exiting! \n");
+			exit(1);
+		}
+ 
+		printf("Opened %s \n", argv[1]);
 
-	FILE* output = fopen(argv[2], "wb");
-        if (!output) abort();
-	printf("Opened %s \n", argv[2]);
+		FILE* output = fopen(argv[2], "wb");
+        if (!output)
+		{
+			printf("Couldn't open %s \n", argv[2]);
+			printf("Exiting! \n");
+			exit(1);
+		}
+		printf("Opened %s \n", argv[2]);
 
         size_t count;
 	
-	char READ_HEADER[sizeof(HEADER)];
+		char READ_HEADER[sizeof(HEADER)];
 	
         count = fread(READ_HEADER, 1, sizeof(READ_HEADER), file);
 
-	if(!strncmp(HEADER,READ_HEADER,strlen(HEADER)))
-	{
-	    abort();
-	}
-	else
-	{
-	    printf("HEADERS match\n");
-	    printf("HEADER='%s'\n", HEADER);
-	}	
+		if(!strncmp(HEADER,READ_HEADER,strlen(HEADER)))
+		{
+	   	 printf("HEADERS don't match! \n");
+		 printf("Exiting! \n");
+		 exit(2);
+		}
+		else
+		{
+	    	printf("HEADERS match\n");
+	    	printf("HEADER='%s'\n", HEADER);
+		}	
 
         count = fread(FirmwareDescription, FIRMWARE_DESCRIPTION_SIZE, 1, file);
         printf("FirmwareDescription='%s'\n", FirmwareDescription);
 
-	fseek ( file , 16 , SEEK_CUR); //mkfw writes 16 bytes offset after the description, set file postion 16 byte forward
+		fseek ( file , 16 , SEEK_CUR); //mkfw writes 16 bytes offset after the description, set file postion 16 byte forward
 	
         count = fread(tile, 1, sizeof(tile), file);
         if (count != sizeof(tile))
         {
-            printf("invalid tile \n");
-            abort();
+            printf("Invalid tile\n");
+			printf("Exiting! \n");
+            exit(3);
         }
 	
-	count = fwrite(tile, 1, sizeof(tile), output);
+		count = fwrite(tile, 1, sizeof(tile), output);
         printf("tile: wrote %d bytes.\n", (int)count);
 
     }
