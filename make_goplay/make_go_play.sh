9defcea64128a8ecbe888e0307db962b1bc347e4
cynr1c <jann+gl@doomlocks.de>
2019-08-13 23:23:48 +0200

    customize for mega go play

--- make_goplay/make_go_play.sh
+++ make_goplay/make_go_play.sh
@@ -9,12 +9,12 @@
 
 export PATH="$PATH:$HOME/esp/xtensa-esp32-elf/bin"
 export IDF_PATH=~/esp/esp-idf
-GOPLAY_SOURCE_PATH=""
+GOPLAY_SOURCE_PATH=~/Schreibtisch/odgo/super-go-play
 PATH_ORIG=`pwd`
 SERIAL_PORT="/dev/ttyUSB0"
 GO_PLAY_ORIG_FW_PATH="Go-Play.fw"
-EFWT_PATH="../efwt" 
-MKFW_PATH="."
+EFWT_PATH="$PATH_ORIG/../efwt" 
+MKFW_PATH="$PATH_ORIG/../../odroid-go-firmware/tools/mkfw"
 
 echo "Compiling Gnuboy"
 cd $GOPLAY_SOURCE_PATH/gnuboy-go
@@ -45,6 +45,7 @@ echo "Building mkfw"
 cd $MKFW_PATH
 make 
 
+cd $PATH_ORIG
 if [ ! -f tile.raw ]; then
 	cd $EFWT_PATH
 	make
@@ -57,8 +58,8 @@ if [ ! -f tile.raw ]; then
 fi
 
 cd $PATH_ORIG
-$MKFW_PATH/mkfw Go-Play tile.raw 0 16 1048576 springboard springboard.bin 0 17 1048576 nesemu nesemu-go.bin 0 18 1048576 gnuboy gnuboy-go.bin 0 19 2097152 smsplusgx smsplusgx-go.bin
-mv firmware.fw Go-Play-New.fw
+$MKFW_PATH/mkfw Mega-Go-Play tile.raw 0 16 1048576 springboard springboard.bin 0 17 1048576 nesemu nesemu-go.bin 0 18 1048576 gnuboy gnuboy-go.bin 0 19 2097152 smsplusgx smsplusgx-go.bin
+mv firmware.fw Mega-Go-Play.fw
 
 #To convert the picture into a png run:
 #ffmpeg -f rawvideo -pixel_format rgb565 -video_size 86x48 -i tile.raw output.png
